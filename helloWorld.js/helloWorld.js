1d407233da0bee13927da292568837287703d01b
TufSheep <deonholl@ymail.com>
2022-09-20 21:06:20 +0200

    Added the user input option

--- helloWorld.js/helloWorld.js
+++ helloWorld.js/helloWorld.js
@@ -1,2 +1,8 @@
 let message = 'Git is Awesome!';
 console.log(message);
+
+function myFunction() {
+let newPrompt = prompt("What is your favourite programming language?");
+alert("My favourite programming language is " + newPrompt);
+}
+myFunction();
