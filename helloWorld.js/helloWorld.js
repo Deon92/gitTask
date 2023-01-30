5df57a37c56cc0951c32a32c7f97e5ed9eb2a3c7
Deon <57843686+Deon92@users.noreply.github.com>
2023-01-30 16:33:15 +0200

    Update helloWorld.js

--- helloWorld.js/helloWorld.js
+++ helloWorld.js/helloWorld.js
@@ -1,6 +1,9 @@
 let message = 'Git is Awesome!';
 console.log(message);
 
+const testConsole = 'Hello World';
+console.log(testConsole);
+
 function myFunction() {
 let newPrompt = prompt("What is your favourite programming language?");
 alert("My favourite programming language is " + newPrompt);
